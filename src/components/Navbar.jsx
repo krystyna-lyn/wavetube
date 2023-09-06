646380c4b7b79a49dfc73b00e42b63cce651345d
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-09-06 20:22:05 +0100

    add utils

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import {Link} from 'react-router-dom';
+import {Stack} from '@mui/material';
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
+import {logo} from '../utils/constants'
+
+const Navbar = () => 
+ (
+    <Stack direction="row"
+     alignItems="center"
+    sx={{position: 'sticky', backgroundColor: '#000',
+    top: '0',justifyContent: 'space-between'}} >
+
+      <Link to="/" style={{display: 'flex', alignItems: 'center'}}>
+        <img src={logo} alt='logo' height={45}/>
+     
+      </Link>
+    </Stack>
   )
-}
+
 
 export default Navbar
