ea67a2b50c455605ce700eace1abb267a4f6f565
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-09-13 21:53:36 +0100

    describing ChannelDetail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import {Videos, ChannelCard} from './';
+import { fetchAPI } from "../utils/fetchAPI";
 
 const ChannelDetail = () => {
+
+const {id} =  useParams();
+const {channelDetail, setChannelDetail} = useState(null);
+const {videos, setVideos} = useState([]);
+
+useEffect(()=>{
+fetchAPI(`channels?part=snippet&id=${id}`)
+.then((data)=>setChannelDetail(data?.items[0]))
+
+fetchAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`)
+.then((data)=>setVideos(data?.items[0]))
+},{id})
+
+
   return (
-    <div>ChannelDetail</div>
+    <Box>
+
+    </Box>
   )
 }
 
