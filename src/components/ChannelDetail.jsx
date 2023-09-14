5c9c17856ae93f146bae6c29c89538ae1a9383ef
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-09-14 16:09:58 +0100

    styles VideoDetail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
-import {Videos, ChannelCard} from './';
+import { Videos, ChannelCard } from './';
 import { fetchAPI } from "../utils/fetchAPI";
 
 const ChannelDetail = () => {
@@ -26,11 +26,22 @@ const ChannelDetail = () => {
   }, [id]);
 
 
-
-
   return (
-    <Box>
+    <Box minHeight='95vh'>
+      <Box>
+        <div style={{
+          height: '300px',
+          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+          zIndex: 10
+        }} />
+        <ChannelCard channelDetail={channelDetail }
+        marginTop='-110px'/>
 
+        <Box display='flex' p='2'>
+          <Box sx={{mr: {sm: '100px'}}}/>
+            <Videos videos={videos}/>
+        </Box>
+      </Box>
     </Box>
   )
 }
